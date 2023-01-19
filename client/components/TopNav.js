c868403d6f2d0ec6ac54c39fbed94338d9e3c9a4
trismagp <tris.magp@gmail.com>
2023-01-19 21:19:57 +0100

    update TopNav

--- client/components/TopNav.js
+++ client/components/TopNav.js
@@ -1,10 +1,35 @@
+import { Menu } from "antd";
+import Link from "next/link";
+import {
+  AppstoreOutlined,
+  LoginOutlined,
+  UserAddOutlined,
+} from "@ant-design/icons";
+
+const { Item } = Menu;
+
 const TopNav = () => {
-    return (
-      <>
-        <p>Top nav</p>
-      </>
-    );
-  };
-  
-  export default TopNav;
-  
+  return (
+    <Menu mode="horizontal">
+      <Item icon={<AppstoreOutlined />}>
+        <Link href="/">
+          App
+        </Link>
+      </Item>
+
+      <Item icon={<LoginOutlined />}>
+        <Link href="/login">
+          Login
+        </Link>
+      </Item>
+
+      <Item icon={<UserAddOutlined />}>
+        <Link href="/register">
+          Register
+        </Link>
+      </Item>
+    </Menu>
+  );
+};
+
+export default TopNav;
