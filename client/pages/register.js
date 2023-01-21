89d3175f2900be377e7e00ee618e131e4370320d
trismagp <tris.magp@gmail.com>
2023-01-21 15:30:49 +0100

    add .env.local

--- client/pages/register.js
+++ client/pages/register.js
@@ -14,7 +14,9 @@ const Register = () => {
     // console.table({ name, email, password });
     try {
       setLoading(true);
-      const { data } = await axios.post(`http://localhost:8000/api/register`, {
+      const { data } = await axios.post(
+        `${process.env.NEXT_PUBLIC_API}/register`, 
+      {
         name,
         email,
         password,
