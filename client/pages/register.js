d0d3ef758065f95ece6207c67bff562e246fa3f6
trismagp <tris.magp@gmail.com>
2023-01-21 15:12:01 +0100

    add react-toastify

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 
 const Register = () => {
@@ -10,12 +11,17 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     // console.table({ name, email, password });
-    const { data } = await axios.post(`http://localhost:8000/api/register`, {
-      name,
-      email,
-      password,
-    });
-    console.log("REGISTER RESPONSE", data);
+    try {
+      const { data } = await axios.post(`http://localhost:8000/api/register`, {
+        name,
+        email,
+        password,
+      });
+      // console.log("REGISTER RESPONSE", data);
+      toast("Registration successful. Please login.");
+    } catch (err) {
+      toast.error(err.response.data);
+    }
   };
 
   return (
