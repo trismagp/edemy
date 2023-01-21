86ce3b80c373a93be1e0f90fa7ee5b82c8afa06a
trismagp <tris.magp@gmail.com>
2023-01-21 15:23:17 +0100

    add spinner

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,17 +1,19 @@
 import { useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
-
+import { SyncOutlined } from "@ant-design/icons";
 
 const Register = () => {
   const [name, setName] = useState("Ryan");
   const [email, setEmail] = useState("ryan@gmail.com");
   const [password, setPassword] = useState("rrrrrr");
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     // console.table({ name, email, password });
     try {
+      setLoading(true);
       const { data } = await axios.post(`http://localhost:8000/api/register`, {
         name,
         email,
@@ -19,8 +21,10 @@ const Register = () => {
       });
       // console.log("REGISTER RESPONSE", data);
       toast("Registration successful. Please login.");
+      setLoading(false);
     } catch (err) {
       toast.error(err.response.data);
+      setLoading(false);
     }
   };
 
@@ -57,8 +61,12 @@ const Register = () => {
             required
           />
 
-          <button type="submit" className="btn btn-block btn-primary">
-            Submit
+          <button 
+            type="submit" 
+            className="btn btn-block btn-primary"
+            disabled={!name || !email || !password || loading}
+          >
+            {loading ? <SyncOutlined spin /> : "Submit"}
           </button>
         </form>
       </div>
