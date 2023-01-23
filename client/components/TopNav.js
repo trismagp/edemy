7e5468a957c7bc375729ffb5a13874a23aa4b21a
trismagp <tris.magp@gmail.com>
2023-01-24 00:02:08 +0100

    add logout

--- client/components/TopNav.js
+++ client/components/TopNav.js
@@ -1,22 +1,37 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { Menu } from "antd";
 import Link from "next/link";
 import {
   AppstoreOutlined,
   LoginOutlined,
+  LogoutOutlined,
   UserAddOutlined,
 } from "@ant-design/icons";
+import { Context } from "../context";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 
 const { Item } = Menu;
 
 const TopNav = () => {
   const [current, setCurrent] = useState("");
 
-  let isServer = typeof window === 'undefined' ? false : true;
+  const { state, dispatch } = useContext(Context);
+
+  const router = useRouter();
+
   useEffect(() => {
-    isServer && setCurrent(window.location.pathname);
-  }, [isServer && window.location.pathname]);
-  
+    process.browser && setCurrent(window.location.pathname);
+  }, [process.browser && window.location.pathname]);
+
+  const logout = async () => {
+    dispatch({ type: "LOGOUT" });
+    window.localStorage.removeItem("user");
+    const { data } = await axios.get("/api/logout");
+    toast(data.message);
+    router.push("/login");
+  };
 
   return (
     <Menu mode="horizontal" selectedKeys={[current]}>
@@ -48,6 +63,10 @@ const TopNav = () => {
         <Link href="/register">
           Register
         </Link>
+      </Item>
+
+      <Item onClick={logout} icon={<LogoutOutlined />} className="float-right">
+        Logout
       </Item>
     </Menu>
   );
