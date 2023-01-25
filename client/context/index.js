4e88c21edf12c0f9f596d34b58c3efa482c78da8
trismagp <tris.magp@gmail.com>
2023-01-25 21:08:12 +0100

    handle expired token, csrf protection, verify token and get current user

--- client/context/index.js
+++ client/context/index.js
@@ -1,4 +1,8 @@
 import { useReducer, createContext, useEffect } from "react";
+import axios from "axios";
+import { useRouter, userRouter } from "next/router";
+
+
 
 // initial state
 const intialState = {
@@ -29,6 +33,45 @@ const Provider = ({ children }) => {
       type: "LOGIN",
       payload: JSON.parse(window.localStorage.getItem("user")),
     });
+  }, []);
+
+  axios.interceptors.response.use(
+    function (response) {
+      // any status code that lie within the range of 2XX cause this function
+      // to trigger
+      return response;
+    },
+    function (error) {
+      // any status codes that falls outside the range of 2xx cause this function
+      // to trigger
+      let res = error.response;
+      if (res.status === 401 && res.config && !res.config.__isRetryRequest) {
+        return new Promise((resolve, reject) => {
+          axios
+            .get("/api/logout")
+            .then((data) => {
+              console.log("/401 error > logout");
+              dispatch({ type: "LOGOUT" });
+              window.localStorage.removeItem("user");
+              router.push("/login");
+            })
+            .catch((err) => {
+              console.log("AXIOS INTERCEPTORS ERR", err);
+              reject(error);
+            });
+        });
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  useEffect(() => {
+    const getCsrfToken = async () => {
+      const { data } = await axios.get("/api/csrf-token");
+      //console.log("CSRF", data);
+      axios.defaults.headers["X-CSRF-Token"] = data.getCsrfToken;
+    };
+    getCsrfToken();
   }, []);
 
   return (
